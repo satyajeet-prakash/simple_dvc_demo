f1e04807dd1c6c3522531dcf3a1ce9d13de9ceec
Satyajeet <sant8430@gmail.com>
2022-03-26 11:32:46 +0530

    added get_data

--- src/get_data.py
+++ src/get_data.py
@@ -18,7 +18,6 @@ def get_data(config_path):
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
 
-
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
