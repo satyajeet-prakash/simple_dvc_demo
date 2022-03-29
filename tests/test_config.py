52e8a59c701db8f9e2f6a97e454b154b58e5f0c7
Satyajeet <sant8430@gmail.com>
2022-03-30 01:48:05 +0530

    test done

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,10 @@
+from email import message
 import json
 import logging
 import os
 import joblib
 import pytest
+from prediction_service import prediction
 from prediction_service.prediction import form_response, api_response
 import prediction_service
 
@@ -64,3 +66,25 @@ def test_form_response_correct_range(data=input_data["correct_range"]):
 def test_api_response_correct_range(data=input_data["correct_range"]):
     res = api_response(data)
     assert TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
+
+
+def test_form_response_incorrect_range(data=input_data["incorrect_range"]):
+    with pytest.raises(prediction_service.prediction.NotInRange):
+        form_response(data)
+
+
+def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInRange(
+    ).message
+
+
+def test_form_response_incorrect_col(data=input_data["incorrect_col"]):
+    with pytest.raises(prediction_service.prediction.NotInCols):
+        form_response(data)
+
+
+def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInCols(
+    ).message
